43788d43a74935df5b11cb68b8288ee813fa4ad3
Emrebsr21 <emrebaser7@hotmail.com>
2025-01-07 08:14:44 +0300

    Add files via upload

---
+++ EmreBaserStairs/ClassLibrary1/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace ClassLibrary1
+{
+
+    using static System.Net.Mime.MediaTypeNames;
+
+
+    public class Stairs
+    {
+        private Thread stairsThread;
+        private bool running;
+        private bool paused;
+        private readonly object pauseLock = new object();
+
+        public Stairs()
+        {
+            stairsThread = new Thread(PrintStairs);
+            running = false;
+            paused = false;
+        }
+        public void Start()
+        {
+            if (stairsThread.ThreadState == ThreadState.Unstarted || stairsThread.ThreadState == ThreadState.Stopped)
+            {
+                stairsThread = new Thread(PrintStairs);
+                stairsThread.Start();
+                Console.WriteLine("Starting Stairs now:");
+            }
+            else if (paused)
+            {
+                Resume();
+            }
+        }
+
+        public void Pause()
+        {
+            lock (pauseLock)
+            {
+                paused = true;
+                Console.WriteLine("Stairs have been paused:");
+            }
+        }
+
+        public void Resume()
+        {
+            lock (pauseLock)
+            {
+                paused = false;
+                Monitor.Pulse(pauseLock);
+                Console.WriteLine("Stairs have been started from beginning:");
+            }
+        }
+
+        public void Stop()
+        {
+            running = false;
+            if (paused) Resume(); 
+            stairsThread.Join();
+        }
+
+        private void PrintStairs()
+        {
+            running = true;
+            int step = 0;
+            while (running)
+            {
+                lock (pauseLock)
+                {
+                    if (paused)
+                    {
+                        Monitor.Wait(pauseLock);
+                    }
+                }
+
+                if (!running) break;
+                //Console.WriteLine();
+                Console.WriteLine(new string(' ', step) + "|_");
+                step++;
+                Thread.Sleep(500); 
+            }
+        }
+
+        public void Restart()
+        {
+            Stop();
+            stairsThread = new Thread(PrintStairs);
+            stairsThread.Start();
+        }
+    }
+}
+
---
+++ EmreBaserStairs/EmreBaserStairs/Program.cs
@@ -0,0 +1,49 @@
+﻿using ClassLibrary1;
+using System;
+using System.Threading;
+
+namespace EmreBaserStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stairs stairs = new Stairs();
+            bool exit = false;
+
+            Console.WriteLine("Press s to start or resume Stairs");
+            Console.WriteLine("Press x to pause Stairs");
+            Console.WriteLine("Press r to start the Stairs from the beginning");
+            Console.WriteLine("Press q to quit the application");
+
+            while (!exit)
+            {
+                Console.WriteLine();
+                var keyInfo = Console.ReadKey(intercept: true);
+                char choice = keyInfo.KeyChar;
+                Console.WriteLine(choice);
+                 
+                switch (choice)
+                {
+                    case 's':
+                        stairs.Start();
+                        break;
+                    case 'x':
+                        stairs.Pause();
+                        break;
+                    case 'r':
+                        stairs.Restart();
+                        break;
+                    case 'q':
+                        stairs.Stop();
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        Thread.Sleep(2000); 
+                        break;
+                }
+            }
+        }
+    }
+}
